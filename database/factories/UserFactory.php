498cf5ba63375d772255008f6007beb3d73a3e8c
ckrek <beloborodovnazar3381@gmail.com>
2026-03-11 09:10:07 +0400

    first commit

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,31 +7,20 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends Factory<User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    public function definition()
     {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => bcrypt('password'), // пароль по умолчанию
             'remember_token' => Str::random(10),
+            'role' => 'user', // или 'admin' если нужно
         ];
     }
+
 
     /**
      * Indicate that the model's email address should be unverified.
